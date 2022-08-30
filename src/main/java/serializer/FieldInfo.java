35c4586a9da56f03b32c133b91643a0d12510df9
alex <a.glasov@yandex.ru>
2022-08-31 00:02:09 +0300

    remove unused method

--- src/main/java/serializer/FieldInfo.java
+++ src/main/java/serializer/FieldInfo.java
@@ -72,16 +72,6 @@ public class FieldInfo {
         return field.get(object);
     }
 
-    public FieldInfo withOptional(boolean isOptional) {
-        return new FieldInfo(
-                field,
-                type,
-                isOptional,
-                isPrimitive,
-                deserializer
-        );
-    }
-
     @Override
     public String toString() {
         return "FieldInfo{" +
